27bd84412810719cff0247c130d2d9b23806dd55
Dave Barthly <davebarthly@gmail.com>
2016-09-23 20:07:37 -0400

    Initialize repo

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+// window.onload = function () {
+//
+// var context = new window.AudioContext() || new window.webkitAudioContext();
+//
+//   osc = context.createOscillator();
+//   vol = context.createGain();
+//   volControl = document.getElementById("volume")
+//   osc.frequency.value = 180;
+//   osc.connect(context.destination);
+//
+//   osc.type = 'square'
+//   osc.connect(vol)
+//
+//   osc.start()
+//
+//
+// }
